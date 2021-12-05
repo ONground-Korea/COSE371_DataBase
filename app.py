fb46179197e9154b7000f21e847db61f0afcf481
Minkyu Baik <0713jake@naver.com>
2021-12-06 02:38:48 +0900

    .

--- app.py
+++ app.py
@@ -1,19 +1,43 @@
 from flask import *
 from flask_bootstrap import Bootstrap
+import psycopg2
+import psycopg2.extras
+import hashlib
 
 app = Flask(__name__)
 Bootstrap(app)
+
+#connect=psycopg2.connect("dbname=원하는거 user=postgres password=알아서")
+#cur=connect.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+### Frontend ###
 
 @app.route('/program/<name>')
 def program(name):
     return render_template('bootstrap.html', name=name)
 
-@app.route('/index')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/login')
 def login():
+    return render_template('login.html')
+
+### Backend ###
+
+@app.route('/backend/login', methods=['POST'])
+def blogin():
+    id=request.form['id']
+    password=request.form['password']
+    print(id, password)
+    #cur.execute('SELECT * FROM students WHRER id=%s AND password=%s'%(id, hashlib.sha512(password.encode()).hexdigest()))
+    #result=cur.fetchall()
+    #if len(result)!=1:
+    #    flash('No Matching Information')
+    #    return render_template('login.html')
+    #else:
+    #    return render_template('.html', user=result['id'])
     return render_template('login.html')
 
 if __name__ == '__main__':
