ff626fe48a43a5b5b0e80f67996e1593ef223e03
Minkyu Baik <0713jake@naver.com>
2021-12-09 02:07:30 +0900

    .

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.secret_key = "htmlisnotaprogramminglanguage"
 Bootstrap(app)
 
-connect = psycopg2.connect("dbname=termproject user=postgres password=wkdrnwkdrn1@ client_encoding=utf8")
+connect = psycopg2.connect("dbname=sugang user=postgres password=0000 client_encoding=utf8")
+connect.autocommit=True
 cur = connect.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 user=None
 
@@ -38,39 +39,8 @@ def mycourses():
 
 @app.route('/allcourses', methods=['GET', 'POST'])
 def allcourses():
-    query = 'SELECT * FROM section JOIN instructor ON section.instructor_id=instructor.instructor_id, course JOIN department ON course.dept_name=department.dept_name WHERE course.course_id=section.course_id'
+    query = 'SELECT * FROM (section JOIN instructor ON section.instructor_id=instructor.instructor_id) AS temp1, (course JOIN department ON course.dept_name=department.dept_name) AS temp2 WHERE temp2.course_id=temp1.course_id'
     if request.method == 'POST':
-<<<<<<< HEAD
-        print(request.form)
-        year = request.form['pYear']
-        semester = request.form['pTerm']
-        college = request.form['pCol']
-        credit = request.form['pCredit']
-        instructor = request.form['pProf']
-        course_id = request.form['pCourCd']
-        section_id = request.form['pCourCls']
-        course_name = request.form['pCourNm']
-        # TODO
-        if year != '':
-            query += " AND section.year=%d" % int(year)
-        if semester != '':
-            query += " AND section.semester='%s'" % semester
-        if college != '':
-            if college=='교양':
-                query+=" AND course.type='elective'"
-            else:
-                query+=" AND course.type<>'elective' AND course.dept_name='%s'"%college
-        if credit != '':
-            query+=" AND course.credit=%d"%credit
-        if instructor != '':
-            query+=" AND section.instructor_name LIKE '%%%s%%'"%instructor
-        if course_id != '':
-            query+=" AND course.course_id LIKE '%%%s%%'"%course_id
-        if section_id != '':
-            query+=" AND section.section_id LIKE '%%%s%%'"%section_id
-        if course_name != '':
-            query+=" AND course.course_name LIKE '%%%s%%'"%course_name
-=======
         if request.form.get('submit') == "submit":
             year = request.form['pYear']
             semester = request.form['pTerm']
@@ -80,30 +50,27 @@ def allcourses():
             course_id = request.form['pCourCd']
             section_id = request.form['pCourCls']
             course_name = request.form['pCourNm']
-            print(year, semester, college, credit, instructor, course_id, section_id, course_name)
             # TODO
             if year != '':
-                query += " AND section.year=%d" % int(year)
+                query += " AND temp1.year=%d" % int(year)
             if semester != '':
-                query += " AND section.semester='%s'" % semester
+                query += " AND temp1.semester='%s'" % semester
             if college != '':
                 if college=='교양':
-                    query+=" AND course.type='elective'"
+                    query+=" AND temp2.type='elective'"
                 else:
-                    query+=" AND course.type<>'elective' AND course.dept_name='%s'"%college
+                    query+=" AND temp2.type<>'elective' AND temp2.college_name='%s'"%college
             if credit != '':
-                query+=" AND course.credit=%d"%credit
+                query+=" AND temp2.credits=%d"%int(credit)
             if instructor != '':
-                query+=" AND section.instructor_name LIKE '%%%s%%'"%instructor
+                query+=" AND temp1.instructor_name LIKE '%%%s%%'"%instructor
             if course_id != '':
-                query+=" AND course.course_id LIKE '%%%s%%'"%course_id
+                query+=" AND temp2.course_id LIKE '%%%s%%'"%course_id
             if section_id != '':
-                query+=" AND section.section_id LIKE '%%%s%%'"%section_id
+                query+=" AND temp1.section_id LIKE '%%%s%%'"%section_id
             if course_name != '':
-                query+=" AND course.course_name LIKE '%%%s%%'"%course_name
-
->>>>>>> 1f3d8c13a00a1409e43ba17a5fa7851c9b77c7ff
-    query+=' ORDER BY section.course_id, section.section_id ASC;'
+                query+=" AND temp2.course_name LIKE '%%%s%%'"%course_name
+    query+=' ORDER BY temp1.course_id, temp1.section_id ASC;'
     cur.execute(query)
     result = cur.fetchall()
     modified=[]
@@ -126,7 +93,7 @@ def allcourses():
         query2+=' ORDER BY timeslot_id ASC;'
         cur.execute(query2)
         temp=cur.fetchall()
-        if len(temp)>0:
+        if request.method != 'POST' or request.method == 'POST' and (day!='' or time!='') and len(temp)>0 or request.method == 'POST' and day=='' and time=='':
             modified.append(i)
     return render_template('allcourses.html', courses=modified)
 
@@ -191,7 +158,7 @@ def register():
     std_id = request.form['std_id']
     id = request.form['id']
     password = request.form['password']
-    cur.execute("SELECT COUNT(std_id) WHERE std_id='%s'"%std_id)
+    cur.execute("SELECT std_id FROM login WHERE std_id='%s'"%std_id)
     result=cur.fetchall()
     if len(result)==1:
         cur.execute(
@@ -208,11 +175,11 @@ def register():
 def admin():
     # TODO
     # 기본 검색 기능은 일반 유저와 동일하게 가능하도록.
-
     if request.method == 'POST':
-        if request.form.get('submit') == "submit":
+        if request.form.get('submit2') == "submit2":
             # TODO
             # 과목 추가 기능
+            print(request.form)
             course_id = request.form['course_id']
             section_id = request.form['section_id']
             course_name = request.form['course_name']
@@ -221,19 +188,18 @@ def admin():
             credits = request.form['credits']
             hour = request.form['hour']
             year = request.form['year']
-            semester = request.form['semeseter']
+            semester = request.form['semester']
             instructor_id = request.form['instructor_id']
             cur.execute(
                 "INSERT INTO course VALUES ('%s', '%s', '%s', '%s', '%s', '%s');" % (
                     course_id, course_name, dept_name, type, credits, hour)
             )
-            cur.commit()
             cur.execute(
-                "INSERT INTO section VALUES ('%s','%s','%s','%s','%s','%s');" % (
+                "INSERT INTO section VALUES (%s,'%s','%s','%s','%s','%s');" % (
                     "DEFAULT", course_id, section_id, year, semester, instructor_id)
             )
-            cur.commit()
             flash('Added')
+            return render_template('admin.html')
         elif request.form.get('delete') == "delete":
             # 과목 삭제 기능
             dcourse_id = request.form['dcourse_id']
@@ -243,72 +209,66 @@ def admin():
             cur.execute(
                 "DELETE FROM section WHERE (course_id, section_id, year, semester) = ('%s','%s', '%s','%s')" %(dcourse_id, dsection_id, dyear, dsemester)
             )
-            cur.commit()
             flash('Deleted')
+            return render_template('admin.html')
         else:
-            query = 'SELECT * FROM section JOIN instructor ON section.instructor_id=instructor.instructor_id, course JOIN department ON course.dept_name=department.dept_name WHERE course.course_id=section.course_id'
-            year = request.form['pYear']
-            semester = request.form['pTerm']
-            college = request.form['pCol']
-            credit = request.form['pCredit']
-            instructor = request.form['pProf']
-            course_id = request.form['pCourCd']
-            section_id = request.form['pCourCls']
-            course_name = request.form['pCourNm']
-            # TODO
-            if year != '':
-                query += " AND section.year=%d" % int(year)
-            if semester != '':
-                query += " AND section.semester='%s'" % semester
-            if college != '':
-                if college == '교양':
-                    query += " AND course.type='elective'"
-                else:
-                    query += " AND course.type<>'elective' AND course.dept_name='%s'" % college
-            if credit != '':
-                query += " AND course.credit=%d" % credit
-            if instructor != '':
-                query += " AND section.instructor_name LIKE '%%%s%%'" % instructor
-            if course_id != '':
-                query += " AND course.course_id LIKE '%%%s%%'" % course_id
-            if section_id != '':
-                query += " AND section.section_id LIKE '%%%s%%'" % section_id
-            if course_name != '':
-                query += " AND course.course_name LIKE '%%%s%%'" % course_name
-            query += ' ORDER BY section.course_id, section.section_id ASC;'
+            query = 'SELECT * FROM (section JOIN instructor ON section.instructor_id=instructor.instructor_id) AS temp1, (course JOIN department ON course.dept_name=department.dept_name) AS temp2 WHERE temp2.course_id=temp1.course_id'
+            if request.form.get('submit') == "submit":
+                year = request.form['pYear']
+                semester = request.form['pTerm']
+                college = request.form['pCol']
+                credit = request.form['pCredit']
+                instructor = request.form['pProf']
+                course_id = request.form['pCourCd']
+                section_id = request.form['pCourCls']
+                course_name = request.form['pCourNm']
+                # TODO
+                if year != '':
+                    query += " AND temp1.year=%d" % int(year)
+                if semester != '':
+                    query += " AND temp1.semester='%s'" % semester
+                if college != '':
+                    if college=='교양':
+                        query+=" AND temp2.type='elective'"
+                    else:
+                        query+=" AND temp2.type<>'elective' AND temp2.college_name='%s'"%college
+                if credit != '':
+                    query+=" AND temp2.credits=%d"%int(credit)
+                if instructor != '':
+                    query+=" AND temp1.instructor_name LIKE '%%%s%%'"%instructor
+                if course_id != '':
+                    query+=" AND temp2.course_id LIKE '%%%s%%'"%course_id
+                if section_id != '':
+                    query+=" AND temp1.section_id LIKE '%%%s%%'"%section_id
+                if course_name != '':
+                    query+=" AND temp2.course_name LIKE '%%%s%%'"%course_name
+            query+=' ORDER BY temp1.course_id, temp1.section_id ASC;'
             cur.execute(query)
             result = cur.fetchall()
-            modified = []
+            modified=[]
             for i in range(len(result)):
-                query2 = "SELECT * FROM (section_time LEFT JOIN timeslot ON section_time.timeslot_id=timeslot.id) AS temp LEFT JOIN place ON temp.place_id=place.id WHERE section_id='%s' ORDER BY timeslot_id ASC;" % \
-                         result[i]['id']
+                query2="SELECT * FROM (section_time LEFT JOIN timeslot ON section_time.timeslot_id=timeslot.id) AS temp LEFT JOIN place ON temp.place_id=place.id WHERE section_id='%s' ORDER BY timeslot_id ASC;"%result[i]['id']
                 cur.execute(query2)
-                temp = cur.fetchall()
-                result[i]['timeslot'] = []
+                temp=cur.fetchall()
+                result[i]['timeslot']=[]
                 for j in temp:
-                    result[i]['timeslot'].append((j['day'] + ' ' if j['day'] is not None else '') + (
-                        str(j['period']) + '교시 ' if j['period'] is not None else '') + (
-                                                     j['building'] + ' ' if j['building'] is not None else '') + (
-                                                     j['building_address'] + ' ' if j[
-                                                                                        'building_address'] is not None else '') + (
-                                                     j['place_name'] if j['place_name'] is not None else ''))
+                    result[i]['timeslot'].append((j['day']+' ' if j['day'] is not None else '')+(str(j['period'])+'교시 ' if j['period'] is not None else '')+(j['building']+' ' if j['building'] is not None else '')+(j['building_address']+' ' if j['building_address'] is not None else '')+(j['place_name'] if j['place_name'] is not None else ''))
             for i in result:
-                query2 = "SELECT * FROM (section_time LEFT JOIN timeslot ON section_time.timeslot_id=timeslot.id) AS temp LEFT JOIN place ON temp.place_id=place.id WHERE section_id='%s'" % \
-                         i['id']
+                query2 = "SELECT * FROM (section_time LEFT JOIN timeslot ON section_time.timeslot_id=timeslot.id) AS temp LEFT JOIN place ON temp.place_id=place.id WHERE section_id='%s'" % i['id']
                 day = request.form['pDay']
                 time = request.form['pStartTime']
-                if day != '':
-                    query2 += " AND day='%s'" % day
-                if time != '':
-                    query2 += " AND period=%d" % int(time)
-                query2 += ' ORDER BY timeslot_id ASC;'
+                if day!='':
+                    query2+=" AND day='%s'"%day
+                if time!='':
+                    query2+=" AND period=%d"%int(time)
+                query2+=' ORDER BY timeslot_id ASC;'
                 cur.execute(query2)
-                temp = cur.fetchall()
-                if len(temp) > 0:
+                temp=cur.fetchall()
+                if request.method != 'POST' or request.method == 'POST' and (day!='' or time!='') and len(temp)>0 or request.method == 'POST' and day=='' and time=='':
                     modified.append(i)
             return render_template('admin.html', courses=modified)
-    query = 'SELECT * FROM section JOIN instructor ON section.instructor_id=instructor.instructor_id, course JOIN department ON course.dept_name=department.dept_name WHERE course.course_id=section.course_id'
-    query+=' ORDER BY section.course_id, section.section_id ASC;'
+    query = 'SELECT * FROM (section JOIN instructor ON section.instructor_id=instructor.instructor_id) AS temp1, (course JOIN department ON course.dept_name=department.dept_name) AS temp2 WHERE temp2.course_id=temp1.course_id'
+    query+=' ORDER BY temp1.course_id, temp1.section_id ASC;'
     cur.execute(query)
     result = cur.fetchall()
     modified=[]
@@ -324,7 +284,7 @@ def admin():
         query2+=' ORDER BY timeslot_id ASC;'
         cur.execute(query2)
         temp=cur.fetchall()
-        if len(temp)>0:
+        if request.method != 'POST' or request.method == 'POST' and (day!='' or time!='') and len(temp)>0 or request.method == 'POST' and day=='' and time=='':
             modified.append(i)
     return render_template('admin.html', courses=modified)
 
